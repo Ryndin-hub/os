c1552aa55c56d2666e76e446c15f87fffe8bc9e8
a.ryndin <a.ryndin@g.nsu.ru>
2021-05-21 10:02:09 +0700

    lab5

--- lab5/main.c
+++ lab5/main.c
@@ -6,7 +6,9 @@
 #define NO_FILE_ERROR 1
 #define CANT_OPEN_FILE_ERROR 2
 #define READ_ERROR 3
+#define FGETS_ERROR 4
 #define READ_FAIL -1
+#define REQUESTED_LINE_BUFF_SIZE 10
 
 int main(int argc, char *argv[]) {
     if (argc < 2){
@@ -52,15 +54,36 @@ int main(int argc, char *argv[]) {
         }
         read_result = read(fd, &c, 1);
     }
-    int requested_line;
-    while(scanf("%d", &requested_line)){
+    int requested_line, requested_line_size;
+    char requested_line_str[REQUESTED_LINE_BUFF_SIZE];
+    while(1){
+        char* fgetsResult = fgets(requested_line_str, REQUESTED_LINE_BUFF_SIZE, stdin);
+        if (fgetsResult == NULL){
+            printf("Fgets error\n");
+            exit(FGETS_ERROR);
+        }
+        requested_line_size = strnlen(requested_line_str);
+        if (requested_line_size > 6){
+            printf("Line should be between 0 and %d\n", num_of_lines);
+            continue;
+        }
+        int i, second_continue = 0;
+        for (i = 0; i < requested_line_size - 1; i++){
+            if (requested_line_str[i] < '0' || requested_line_str[i] > '9'){
+                printf("Program needs a number\n");
+                second_continue = 1;
+                break;
+            }
+        }
+        if (second_continue) continue;
+        requested_line = atoi(requested_line_str);
         if (requested_line == 0) return 0;
         if (requested_line < 0 || requested_line > num_of_lines){
             printf("Line should be between 0 and %d\n", num_of_lines);
             continue;
         }
         lseek(fd, file_offsets[requested_line], SEEK_SET);
-        int i;
+        i;
         for (i = 0; i < file_lines_length[requested_line]; i++){
             read(fd, &c, 1);
             printf("%c", c);
